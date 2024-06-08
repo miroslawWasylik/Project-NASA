af69ac4fc625f5b4119c4559541c3e5d3e7d1973
ladenisko <ladenmail@gmail.com>
2024-06-08 18:29:25 +0200

    cors added & planets are loading correctly

--- server/app.js
+++ server/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+
 const planetsRouter = require('./src/routes/planets/planets.router');
 
 const app = express();
+
+app.use(cors({
+	origin: 'http://localhost:3000'
+}));
 app.use(planetsRouter);
 app.use(express.json());
 
