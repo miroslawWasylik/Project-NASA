57bb2ff640e35e36e0664aa1d7987bf0b28def29
ladenisko <ladenmail@gmail.com>
2024-06-08 17:07:39 +0200

    get planets flow added

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const planetsRouter = require('./src/routes/planets/planets.router');
 
 const app = express();
+app.use(planetsRouter);
 app.use(express.json());
 
 module.exports = app;
