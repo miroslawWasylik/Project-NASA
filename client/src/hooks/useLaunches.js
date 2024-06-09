bdfb467e44e8bcd9928653d582df08282bdc582c
ladenisko <ladenmail@gmail.com>
2024-06-09 22:39:06 +0200

    submit launch is working right now

--- client/src/hooks/useLaunches.js
+++ client/src/hooks/useLaunches.js
@@ -34,8 +34,7 @@ function useLaunches(onSuccessSound, onAbortSound, onFailureSound) {
       target,
     });
 
-    // TODO: Set success based on response.
-    const success = false;
+    const success = response.ok;
     if (success) {
       getLaunches();
       setTimeout(() => {
